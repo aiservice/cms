6018c2ee40f6c1393f6423dadac4fbce41a12c3b
yangwu3 <yangwu3@cisco.com>
2021-03-17 16:14:34 +0800

    20210317161614_commit

---
+++ auto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+commit_name="`date +%Y%m%d%H%k%M`_commit"
+echo "====git auto push start..."
+git add .
+git commit -m $commit_name
+git push origin master
+echo "====git auto push end..."
+
