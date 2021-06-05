714fa2a8f6ea32c1f28ab5187aa8e6b1beb0e836
yw3 <yw3@abc.com>
2021-06-05 18:52:02 +0800

    20210605181852_commit

--- scripts/index/site/1952.js
+++ scripts/index/site/1952.js
@@ -6,7 +6,7 @@ g_data_ad_slot_auto = "2215424712";
 g_data_ad_slot_recommend = "6053545297";
 g_data_ad_client = "ca-pub-4603891518763240";
 
-filterUrls = ["xxxxx"];
+filterUrls = ["1115586.html"];
 siteGUrls = ["aiis","good","china","shitou","soft","xiaoli","6tie","lzys","feiku","biquge","ppt","toutiao"];
 siteBUrls = ["aiis","china"];
 go_my_site=true;
