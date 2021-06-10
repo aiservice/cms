0c72911a73b88ff5c94183fc657fd922d66b27a3
yw3 <yw3@abc.com>
2021-06-10 08:45:51 +0800

    aabb

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 commit_name="`date +%Y%m%d%H%k%M`_commit"
-echo "====git auto push start..."
+echo "====git auto push start...$commit_name"
 git add .
 git commit -m $commit_name
 git push origin master
