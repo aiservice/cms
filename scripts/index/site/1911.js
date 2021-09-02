6f8283a1e65ab0e2cf3cf2618c6172fda3f8ea42
yw3 <yw3@abc.com>
2021-09-02 13:19:55 +0800

    20210902_commit

--- scripts/index/site/1911.js
+++ scripts/index/site/1911.js
@@ -1,4 +1,4 @@
-var app_domain = "www.aizhan.club";var is_english = false;var site_id = 1911;var book_total_fee = 0.5;var baid = "a205c14dd8d8e0d7fd00511e984f762f";var gaid = "UA-40716497-15";b_data_ad_mobile = "u3689760";
+var app_domain = "chinaoc.com.cn";var is_english = false;var site_id = 1911;var book_total_fee = 0.5;var baid = "a205c14dd8d8e0d7fd00511e984f762f";var gaid = "UA-40716497-15";b_data_ad_mobile = "u3689760";
 var link_email = "1740933979@qq.com";b_data_ad_336 = "u3689763";
 b_data_ad_336_xuanting = "u3689796";
 
